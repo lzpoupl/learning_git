6dfba8a8cf54d335afa9071b26f59bff506df20d
lzpoup <1853413512@qq.com>
2024-07-31 11:39:17 +0800

    added new feature

--- Main.java
+++ Main.java
@@ -1,7 +1,8 @@
+import java.lang.Scanner
 
 public class Main{
     public static void main(String[] args)
     {
         System.out.printf("Hello, %s","world");
-        
+        Scanner scanner = new Scannner(System.in);
     }
